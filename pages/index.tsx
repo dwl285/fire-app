f297aa8d5cd92a462b3efe6c69a90acf00e4e8b6
DanLee <dwl285@gmail.com>
2021-07-29 15:33:27 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ function Index() {
       <Header />
       <header className="shadow">
         <div className="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
-          <p className="p-4">This will be a description of what the app does</p>
+          <p className="p-4">This will be a description of what the app does, testing PRs!</p>
         </div>
       </header>
       <main>
